f96d03b9d9aa94300cee0ee97a1b1904f35f8262
Rushil Basappa <rushil.oucse37@gmail.com>
2024-04-10 00:17:46 -0700

    testing
    works great
    PCO-123

--- config/nvim/lua/rushinsneaky/lazy.lua
+++ config/nvim/lua/rushinsneaky/lazy.lua
@@ -11,7 +11,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("rushinsneaky.plugins", {
+require("lazy").setup({ { import = "rushinsneaky.plugins" }, { import = "rushinsneaky.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
   change_detection = {
     notify = false
   }
